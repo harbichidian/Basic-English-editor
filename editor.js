ebff62d1786aff5bdca3b863e234fe5d77cec96f
echoback <jtbaker@jtbaker.name>
2012-01-04 09:38:24 -0500

    Historic import

---
+++ editor.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function() {
+	$('#editor').html(localStorage['content']);
+	
+	$('#clear').click(function() {
+			$('#editor').html('');
+			localStorage.clear();
+			window.location = window.location;
+	});
+	
+	$('#editor').on('keypress', function() {
+		if(typeof t === 'undefined') {} else { clearTimeout(t); }
+		var t = setTimeout(editor_timer, 5000);
+	});
+});
+
+function editor_timer() {
+	spellCheck();
+	save();
+}
+
+function save() {
+	var content = $('#editor').html();
+	if(localStorage['content'] != content) {
+		localStorage['content'] = content;
+		
+		var saved = $('#saved');
+		saved.css('opacity', 1);
+		saved.animate({opacity: 0}, 'slow');
+	}
+}
